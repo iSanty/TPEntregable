a9895a01e5b75b1dc44e0d357af4a56b9e0d7918
iSanty <santiago.navalon@hotmail.com>
2022-06-26 18:12:15 -0300

    Agrego mas cosas

--- MTVNavalon/wsgi.py
+++ MTVNavalon/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MTVgrupo project.
+WSGI config for MTVNavalon project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MTVgrupo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MTVNavalon.settings')
 
 application = get_wsgi_application()
