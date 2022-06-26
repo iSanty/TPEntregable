c3aa98a2ec0aca6a31e3a8d7e33eac7c4ba58c59
iSanty <santiago.navalon@hotmail.com>
2022-06-26 13:21:43 -0300

    Primer commit

---
+++ MTVgrupo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MTVgrupo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MTVgrupo.settings')
+
+application = get_wsgi_application()
